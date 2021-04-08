aa465e91a63862c9336043d4ae8b284ef3a4e0bc
Mathéo <matheo.galu56@gmail.com>
2021-04-08 20:49:38 +0200

    Add content class

---
+++ WX Widget test app/wxWidgets_template/CContent.cpp
@@ -0,0 +1,7 @@
+/*
+ * Created on Tue Apr 07 2021
+ *
+ * Copyright (c) 2021 - Mathéo Galuba
+ */
+
+#include "CContent.h"
---
+++ WX Widget test app/wxWidgets_template/CContent.h
@@ -0,0 +1,19 @@
+/*
+ * Created on Tue Apr 07 2021
+ *
+ * Copyright (c) 2021 - Mathéo Galuba
+ */
+
+#pragma once
+
+class CContent
+{
+public:
+	// Public constructor and destructor
+	CContent();
+	~CContent();
+
+protected:
+
+};
+
