ffb3fdee841d91de677f8092819ac86ec68341f7
lechiffre4 <toxcoa@gmail.com>
2021-04-14 18:51:45 +0200

    Begin Network App

--- SFMLApp/SFMLApp/Source.cpp
+++ SFMLApp/SFMLApp/Source.cpp
@@ -1,1 +1,25 @@
-//c'est un test pour savoir si ca fait la merde ou pas
+#include <iostream>
+#include <string>
+#include <SFML/NetWork/Socket.hpp>
+#include <SFML/NetWork/TcpSocket.hpp>
+
+
+
+int main() {
+	//Déclaration de variables et autre
+	sf::TcpSocket Socket1;
+	std::string ipReceiver;
+	int port;
+	std::string name;
+	
+	//Initialisation de la connexion 
+	
+	std::cout << "Entrez l'adresse ip du correspondant : ";
+	std::cin >> ipReceiver;
+	std::cout << "\n Entrez le port de connexion :";
+	std::cin >> port;
+	std::cout << "\n Entrez votre nom : ";
+	std::cin >> name;
+
+
+}
