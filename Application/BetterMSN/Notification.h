00f24f9446616ab49f662ccb4b592349ddc96aef
Mathéo <matheo.galu56@gmail.com>
2021-05-14 15:16:36 +0200

    Add Notification enum

---
+++ Application/BetterMSN/Notification.h
@@ -0,0 +1,16 @@
+/*
+ * Created on Tue Apr 07 2021
+ *
+ * Copyright (c) 2021 - Mathéo G & Sahel H - All Right Reserved
+ *
+ * Licensed under the Apache License, Version 2.0
+ * Available on GitHub at https://github.com/Paracetamol56/Cpp-Network-App
+ */
+
+#pragma once
+
+enum Notification {
+	Notification_Null,
+	Notification_State,
+	Notification_Send
+};
